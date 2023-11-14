ad286485b243aa58dcd493115bc3719a94a12f85
carlA <74275311+carlasalazar@users.noreply.github.com>
2023-11-14 13:31:35 -0600

    Update Main.java

--- Arboles/Main.java
+++ Arboles/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main (String[] args){
         //Arbol binario
         Arbol <String> arbol= new Arbol <> ("A", false);
-        NodoArbol <String> nodoA = new NodoArbol ("A");
+        
         NodoArbol <String> nodoB = new NodoArbol ("B");
         NodoArbol <String> nodoC = new NodoArbol ("C");
         NodoArbol <String> nodoD = new NodoArbol ("D");
@@ -24,22 +24,26 @@ public class Main {
         arbol.agregarNodoArbol(arbol.obtenerRaiz(),nodoB);
         arbol.agregarNodoArbol(arbol.obtenerRaiz(),nodoC);
         arbol.agregarNodoArbol(arbol.obtenerRaiz(),nodoD);
-        /*arbol.agregarNodoArbol(nodoA, nodoC);
-        arbol.agregarNodoArbol(nodoA, nodoD);
+        
         arbol.agregarNodoArbol(nodoB, nodoE);
         arbol.agregarNodoArbol(nodoB, nodoF);
+        
         arbol.agregarNodoArbol(nodoF, nodoJ);
         arbol.agregarNodoArbol(nodoF, nodoK);
         arbol.agregarNodoArbol(nodoF, nodoL);
+        
         arbol.agregarNodoArbol(nodoD, nodoG);
         arbol.agregarNodoArbol(nodoD, nodoH);
         arbol.agregarNodoArbol(nodoD, nodoI);
+        
         arbol.agregarNodoArbol(nodoG, nodoM);
-        arbol.agregarNodoArbol(nodoG, nodoP);
-        arbol.agregarNodoArbol(nodoG, nodoQ);
+        
+        arbol.agregarNodoArbol(nodoM, nodoP);
+        arbol.agregarNodoArbol(nodoM, nodoQ);
+        
         arbol.agregarNodoArbol(nodoI, nodoN);
         arbol.agregarNodoArbol(nodoI, nodoO);
-        */
+        
        
         arbol.imprimirArbol(Recorrido.PREFIJO);
         
